15f66a92ba8a12bd03fd5fe7a2826b1765e24a74
Zeus Tenerife <zeusasico.tenerife@unc.edu.ph>
2025-04-03 08:50:22 +0800

    Update HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,6 +40,7 @@ class HandleInertiaRequests extends Middleware
             'auth.user' => fn() => $request->user() ? $request->user()->only('id', 'full_name', 'email', 'role', 'idNumber') : null,
             'flash' => [
                 'message' => fn() => $request->session()->get('message'),
+                'success' => fn() => $request->session()->get('success'),
             ],
 
         ]);
