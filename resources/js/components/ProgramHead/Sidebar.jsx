feb62c6a9866ae38c3f337d9cdad7f90ec2799d7
Zeus Tenerife <zeusasico.tenerife@unc.edu.ph>
2025-05-09 08:32:30 +0800

    update

--- resources/js/components/ProgramHead/Sidebar.jsx
+++ resources/js/components/ProgramHead/Sidebar.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState, useEffect } from "react";
 import { Link, usePage } from "@inertiajs/react";
 import { getMenuByRole } from "../../config/menuConfig";
--- resources/js/config/menuConfig.js
+++ resources/js/config/menuConfig.js
@@ -5,7 +5,7 @@ import professorMenu from './professorMenu';
 const menuConfig = {
     program_head: programHeadMenu,
     professor: professorMenu, // Add professor menu when available
-    dean: [programHeadMenu], // Add dean menu when available
+    dean: programHeadMenu, // Add dean menu when available
     student: studentMenu,
 };
 
