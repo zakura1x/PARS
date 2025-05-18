3eb93c425584f7421f35750a12508984a6aa871e
Zeus Tenerife <zeusasico.tenerife@unc.edu.ph>
2025-05-18 14:02:47 +0800

    Update shared_route.php

--- routes/shared_route.php
+++ routes/shared_route.php
@@ -88,8 +88,7 @@ Route::middleware(['auth', RoleMiddleware::class . ':professor,program_head,dean
     //Fork the assessment
     Route::post('/assessment/copy/{assessmentId}', [AssessmentCreateController::class, 'forkAssessment'])->name('assessment.fork');
     //Delete the assessment
-    Route::delete('/assessment/delete/
-    {assessmentId}', [AssessmentController::class, 'destroy'])->name('assessment.delete');
+    Route::delete('/assessment/delete/{assessmentId}', [AssessmentController::class, 'destroy'])->name('assessment.delete');
 
     //Assessment Prof View Start of Assessment
     Route::put('/assessment/update/to/wait/{assessmentId}', [AssessmentController::class,'updateToWait'])->name('assessment.update.wait');
